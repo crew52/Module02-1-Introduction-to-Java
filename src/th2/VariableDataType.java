6c84cef40f377ebc84bfab5d1050e136389786e4
crew52 <dungnc150602@gmail.com>
2024-12-05 11:27:47 +0700

    add th2

---
+++ src/th2/VariableDataType.java
@@ -0,0 +1,19 @@
+package th2;
+
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 3.14f;
+        double d = 3.14;
+        boolean b = true;
+        char c = 'A';
+        String str = "Hello";
+
+        System.out.printf("i = %d%n", i);
+        System.out.printf("f = %f%n", f);
+        System.out.printf("d = %f%n", d);
+        System.out.printf("b = %b%n", b);
+        System.out.printf("c = %c%n", c);
+        System.out.printf("str = %s%n", str);
+    }
+}
